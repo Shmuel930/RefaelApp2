9336bc676810bf890fdaff4525a2323da911db07
Shmuel Cohen <ss9300@gmail.com>
2017-07-14 11:35:44 +0300

    Initial commit

---
+++ MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.shmuel.refaelapp2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+<<<<<<< HEAD
+# RefaelAndroid
+=======
+# ProtectorAndroid
+>>>>>>> origin/master
